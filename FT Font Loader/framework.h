b98e5aaa4f8f607312b9f8bd1cccad4e690c21d5
Leadwerks Software <support@leadwerks.com>
2020-02-15 18:08:05 +0200

    Add files via upload

---
+++ FT Font Loader/framework.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#endif
+
+#include "FreeType/include/ft2build.h"
+#include "FreeType/include/freetype/freetype.h"
+#include "FreeType/include/freetype/ftlcdfil.h"
