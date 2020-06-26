bb25d80afa6cf8ae3f69e8d1d5fa1f7c84318f99
Leadwerks Software <support@leadwerks.com>
2020-06-26 08:55:27 +0300

    Add files via upload

--- OBJ Model Saver/main.cpp
+++ OBJ Model Saver/main.cpp
@@ -31,7 +31,8 @@ int GetPluginInfo(unsigned char* cs, int maxsize)
 			"\"title\":\"Wavefront OBJ Model Exporter.\","
 			"\"description\":\"Save models to the Wavefront OBJ file format.\","
 			"\"author\":\"Josh Klint\","
-			"\"saveModelExtensions\":[\"obj\"],"
+			"\"saveModelExtensions\":\"obj\","
+			"\"collapseSavedModels\":true,"
 			"\"saveModelFilter\":\"Wavefront Object (*.obj):obj\""
 		"}"
 	"}";
@@ -44,6 +45,7 @@ MemWriter* writer = NULL;
 //Model load function
 void* SaveModel(void* data, uint64_t size, wchar_t* filepath, uint64_t& returnsize, int flags)
 {
+	std::wstring path = std::wstring(filepath);
 	delete writer;
 	writer = new MemWriter;
 
@@ -52,13 +54,59 @@ void* SaveModel(void* data, uint64_t size, wchar_t* filepath, uint64_t& returnsi
 
 	auto file = new GMFFile;
 	file->Load(&reader);
-	//file->Collapse();
-
+	
 	std::string s;
 	GMFVec3 normal;
+	bool usemtllib = false;
 
+	//Write material library
+	if (!file->materials.empty() and path != L"")
+	{
+		s = "newmtl default\n\n";
+		int materialindex = 1;
+		for (auto mtl : file->materials)
+		{
+			s += "newmtl material" + String(materialindex) + "\n";
+			//"Kd " + String(mtl->color.r) + " " + String(mtl->color.g) + " " + String(mtl->color.b) + "\n";
+			//if (mtl->transparent)
+			//{
+			//	"d " + String(mtl->color.a) + "\n";
+			//} 
+			if (mtl->textures[GMF_TEXTURE_BASE] != 0)
+			{
+				s += "map_Kd " + String(StripDir(file->textures[mtl->textures[GMF_TEXTURE_BASE] - 1]->path)) + "\n";
+			}
+			if (mtl->textures[GMF_TEXTURE_NORMAL] != 0)
+			{
+				s += "map_bump " + String(StripDir(file->textures[mtl->textures[GMF_TEXTURE_NORMAL] - 1]->path)) + "\n";
+				//s += "bump " + String(StripDir(file->textures[mtl->textures[GMF_TEXTURE_NORMAL] - 1]->path)) + "\n";
+				s += "norm " + String(StripDir(file->textures[mtl->textures[GMF_TEXTURE_NORMAL] - 1]->path)) + "\n";
+			}
+			s += "\n";
+			materialindex++;
+		}
+		auto mtlfile = StripExt(path) + L".mtl";
+
+		FILE* file = _wfopen(mtlfile.c_str(), L"wb");
+		if (file != nullptr)
+		{
+			_fseeki64(file, 0, SEEK_SET);
+			size_t l = fwrite(s.c_str(), 1, s.size(), file);
+			if (fclose(file) == 0) usemtllib = true;
+		}
+	}
+
+	//Write object
 	s = "# Exported from ______ Engine\n";
 	writer->Write(s, false);
+
+	if (usemtllib)
+	{
+		s = "\nmtllib " + String(StripAll(path)) + ".mtl\n";
+		writer->Write(s, false);
+	}
+
+	int sumverts = 0;
 
 	for (auto node : file->nodes)
 	{
@@ -70,13 +118,27 @@ void* SaveModel(void* data, uint64_t size, wchar_t* filepath, uint64_t& returnsi
 				{
 					auto mesh = file->meshes[meshindex-1];
 					
+					if (usemtllib)
+					{
+						if (mesh->material != 0)
+						{
+							s = "\nusemtl material" + String(mesh->material) + "\n";
+							writer->Write(s, false);
+						}
+						else
+						{
+							s = "\nusemtl material default\n";
+							writer->Write(s, false);
+						}
+					}
+
 					s = "\n# " + String(int(mesh->vertices.size())) + " vertices\n";
 					writer->Write(s, false);
 					
 					//Vertex positions
 					for (auto& vertex : mesh->vertices)
 					{
-						s = "v " + String(vertex.position.x) + " " + String(vertex.position.y) + " " + String(vertex.position.z) + "\n";
+						s = "v " + String(vertex.position.x) + " " + String(vertex.position.y) + " " + String(-vertex.position.z) + "\n";
 						writer->Write(s, false);
 					}
 					s = "\n";
@@ -85,7 +147,7 @@ void* SaveModel(void* data, uint64_t size, wchar_t* filepath, uint64_t& returnsi
 					//Vertex texcoords
 					for (auto& vertex : mesh->vertices)
 					{
-						s = "vt " + String(halfToFloat(vertex.texcoords[0])) + " " + String(halfToFloat(vertex.texcoords[1])) + "\n";
+						s = "vt " + String(halfToFloat(vertex.texcoords[0])) + " " + String(1.0f - halfToFloat(vertex.texcoords[1])) + "\n";
 						writer->Write(s, false);
 					}
 					s = "\n";
@@ -95,7 +157,7 @@ void* SaveModel(void* data, uint64_t size, wchar_t* filepath, uint64_t& returnsi
 					for (auto& vertex : mesh->vertices)
 					{
 						normal = vertex.GetNormal();
-						s = "vn " + String(normal.x) + " " + String(normal.y) + " " + String(normal.z) + "\n";
+						s = "vn " + String(normal.x) + " " + String(normal.y) + " " + String(-normal.z) + "\n";
 						writer->Write(s, false);
 					}
 
@@ -106,14 +168,16 @@ void* SaveModel(void* data, uint64_t size, wchar_t* filepath, uint64_t& returnsi
 					for (int t = 0; t < mesh->indices.size() / mesh->polyverts; ++t)
 					{
 						s = "f";
-						for (int n = 0; n < mesh->polyverts; ++n)
+						for (int n = mesh->polyverts - 1; n >= 0; --n)
 						{
-							v = int(mesh->indices[t * mesh->polyverts + n]) + 1;
+							v = int(mesh->indices[t * mesh->polyverts + n]) + 1 + sumverts;
 							s += " " + String(v) + "/" + String(v) + "/" + String(v);
 						}
 						s += "\n";
 						writer->Write(s, false);
 					}
+
+					sumverts += mesh->vertices.size();
 				}
 				break;
 			}
