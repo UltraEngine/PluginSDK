977ec9213ed998507c7168121a6c6922e4bfa2dc
Leadwerks Software <support@leadwerks.com>
2022-07-08 06:41:19 +0200

    Add files via upload

--- Plugins/KTXTextureLoader/main.cpp
+++ Plugins/KTXTextureLoader/main.cpp
@@ -33,10 +33,10 @@ int GetPluginInfo(unsigned char* cs, int maxsize)
 			"\"description\":\"Load KTX2 texture format image files.\","
 			"\"author\":\"Josh Klint and Khronos Group\","
 			"\"threadSafe\":true,"
-			//"\"saveTextureExtensions\": [\"ktx2\"],"
+			"\"saveTextureExtensions\": [\"ktx2\"],"
 			"\"loadTextureExtensions\": [\"ktx2\"],"
 			"\"saveTextureFilter\": [\"KTX2 (*.ktx2):ktx2\"]"
-			//"\"loadTextureFilter\": [\"KTX2 (*.ktx2):ktx2\"]"
+			"\"loadTextureFilter\": [\"KTX2 (*.ktx2):ktx2\"]"
 		"}"
 	"}";
 	if (maxsize > 0) memcpy(cs, s.c_str(), min(maxsize,s.length() ) );
@@ -50,6 +50,16 @@ Context::~Context()
 {
 	if (ktx) ktxTexture_Destroy((ktxTexture*)ktx);
 	delete writer;
+}
+
+Context* CreateContext()
+{
+	return new Context;
+}
+
+void FreeContext(Context* context)
+{
+	delete context;
 }
 
 //Texture load function
@@ -95,11 +105,7 @@ void* LoadTexture(Context* context, void* data, uint64_t size, wchar_t* cpath, u
 			ktx_transcode_flags tflags = 0;
 			ktx_transcode_fmt_e tfmt = KTX_TTF_BC7_RGBA;
 			if (ktxTexture2_GetNumComponents(ktx) == 2) tfmt = KTX_TTF_BC5_RG;
-			if (ktxTexture2_TranscodeBasis(ktx, tfmt, tflags) != KTX_SUCCESS)
-			{
-				ktxTexture_Destroy((ktxTexture*)ktx);
-				return NULL;
-			}
+			if (ktxTexture2_TranscodeBasis(ktx, tfmt, tflags) != KTX_SUCCESS) return NULL;
 		}
 	}
 
@@ -114,6 +120,11 @@ void* LoadTexture(Context* context, void* data, uint64_t size, wchar_t* cpath, u
 	if (ktx->isCubemap) texinfo.target = 4;
 	if (ktx->baseDepth > 1) texinfo.target = 3;
 	texinfo.mipmaps = ktx->numLevels;
+	if (ktx->generateMipmaps)
+	{
+		texinfo.genmipmaps = true;
+		texinfo.mipmaps = 1;
+	}
 
 	context->writer = new MemWriter;
 	context->writer->Write(&texinfo);
@@ -122,11 +133,7 @@ void* LoadTexture(Context* context, void* data, uint64_t size, wchar_t* cpath, u
 	{
 		for (int face = 0; face < ktx->numFaces; ++face)
 		{
-			if (ktx->vtbl->GetImageOffset((ktxTexture*)ktx, n, 0, face, &offset) != ktx_error_code_e::KTX_SUCCESS)
-			{
-				ktxTexture_Destroy((ktxTexture*)ktx);
-				return NULL;
-			}
+			if (ktx->vtbl->GetImageOffset((ktxTexture*)ktx, n, 0, face, &offset) != ktx_error_code_e::KTX_SUCCESS) return NULL;
 			auto sz = ktx->vtbl->GetImageSize((ktxTexture*)ktx, n);
 			void* pixels = ktx->pData + offset;
 			int i = sz;
@@ -140,12 +147,64 @@ void* LoadTexture(Context* context, void* data, uint64_t size, wchar_t* cpath, u
 	return context->writer->data();
 }
 
-Context* CreateContext()
+void* SaveTexture(Context* context, wchar_t* extension, const int type, const int width, const int height, const int format, void** mipchain, int* sizechain, const int mipcount, const int layers, uint64_t& returnsize, int flags)
 {
-	return new Context;
-}
+	KTX_error_code result;
+	ktxTextureCreateInfo createInfo = {};
+	createInfo.baseWidth = width;
+	createInfo.baseHeight = height;
+	createInfo.baseDepth = 1;
+	createInfo.numDimensions = 2;
+	createInfo.numLevels = mipcount;
+	createInfo.numLayers = layers;
+	createInfo.numFaces = 1;
+	if (type == 4)
+	{
+		createInfo.numFaces = 6;
+		createInfo.numLayers = 1;
+	}
+	createInfo.isArray = KTX_FALSE;
+	createInfo.generateMipmaps = KTX_FALSE;
+	createInfo.vkFormat = format;
 
-void FreeContext(Context* context)
-{
-	delete context;
+	result = ktxTexture2_Create(&createInfo, KTX_TEXTURE_CREATE_ALLOC_STORAGE, &context->ktx);
+
+	for (int face = 0; face < createInfo.numFaces; ++face)
+	{
+		for (int layer = 0; layer < createInfo.numLayers; ++layer)
+		{
+			for (int miplevel = 0; miplevel < mipcount; miplevel++)
+			{
+				context->ktx->vtbl->SetImageFromMemory((ktxTexture*)context->ktx, miplevel, layer, face, (const ktx_uint8_t*)mipchain[miplevel], sizechain[miplevel]);
+			}
+		}
+	}
+
+	//if ((flags & SAVE_KTX_ASTC_COMPRESSION) != 0)
+	//OETFFunc decode = KHR_DF_TRANSFER_SRGB;
+	//OETFFunc encode = KHR_DF_TRANSFER_Linear;
+	//image->transformOETF(decode, encode);
+
+	ktxBasisParams params = {};
+	params.structSize = sizeof(struct ktxBasisParams);
+	params.compressionLevel = KTX_ETC1S_DEFAULT_COMPRESSION_LEVEL;
+	params.qualityLevel = 128;
+	//params.uastc = true;
+	switch (format)
+	{
+	case VK_FORMAT_BC5_UNORM_BLOCK:
+	case VK_FORMAT_BC5_SNORM_BLOCK:
+	case VK_FORMAT_R8G8_UNORM:
+	case VK_FORMAT_R8G8_SNORM:
+		params.normalMap = true;
+	}
+	auto err = ktxTexture2_CompressBasisEx(context->ktx, &params);
+	if (KTX_SUCCESS != err) return NULL;
+
+	ktx_uint8_t* ppDstBytes = NULL;
+	ktx_size_t pSize = 0;
+	if (KTX_SUCCESS != ktxTexture_WriteToMemory((ktxTexture*)context->ktx, &ppDstBytes, &pSize)) return NULL;
+
+	context->writer->Write(ppDstBytes, pSize);
+	return context->writer->data();
 }
