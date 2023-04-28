7398e40b12521bfeac072a47f9031bbd5e2e5735
UltraEngine <64279130+UltraEngine@users.noreply.github.com>
2023-04-28 16:29:15 +0400

    Delete Plugins/Quake Loader directory

--- Plugins/Quake Loader/Loaders/SPR.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <algorithm>
-#include <vector>
-#include "../Quake.h"
-#include "../DLLExports.h"
-#include "../VKFormat.h"
-
-extern void* LoadTextureSPR(Context* context, void* data, uint64_t size, wchar_t* cpath, uint64_t& returnsize);
--- Plugins/Quake Loader/README.md
+++
@@ -1,3 +0,0 @@
-# Quake Loader
-
-This plugin loads various file formats and packages for the game Quake by id Software.
